245af2e5cef805977b290f7c8a179d9a066939ca
shani <shani@kepempem.com>
2017-08-07 08:40:37 -0400

    Fixed require problem for inner scripts

--- tests/TEST1/server.js
+++ tests/TEST1/server.js
@@ -4,8 +4,11 @@ const Pineapple = require(path.join(__dirname,"..",".."));
 const App = new Pineapple();
 App .public(path.join(__dirname,"public"),"/")
     .set("port",process.env.PORT||5000)
-    // .set("host","0.0.0.0")
+    .set("host","0.0.0.0")
     .on("start",(servers,ports)=>{
         console.log("App is running on port "+ports[0]);
+    })
+    .on("request",()=>{
+        process.stdout.write("|");
     })
     .start();
